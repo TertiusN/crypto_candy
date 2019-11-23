ad39230d4a406925a889b66ab9b1170978b167b4
TertiusN <35367281+TertiusN@users.noreply.github.com>
2019-11-23 01:07:49 +0000

    Initialise Crypto Wallet

--- iota_payment.py
+++ iota_payment.py
@@ -17,34 +17,72 @@ from iota import (
   transaction
 )
 
-node = 'https://tuna.iotasalad.org:14265' #Select your preferred node
+
+
+node = 'https://nodes.thetangle.org:443' #Select your preferred node
 
 parser = ConfigParser()
-config_files = parser.read('config.ini')
-
+config_files = parser.read('wallet.ini')
 cipher_suite = Fernet(os.environ['FERNET_KEY'])
 
 if len(config_files) == 0:
     #Generate random IOTA seed
     seed = ''
-    seed = seed.encode('ascii')
+    seed = seed.encode('utf-8')
     api = Iota(node, seed)
+    iota_seed = api.seed
 
-    iota_seed_e = cipher_suite.encrypt(str(api.seed))
-    print(iota_seed_e)
+    iota_seed_e = cipher_suite.encrypt(bytes(iota_seed))
 
-    cfgfile = open("config.ini",'w')
+    cfgfile = open("wallet.ini",'w')
     parser.add_section('wallet')
-    parser.set('wallet', 'iotaSeed', iota_seed_e)
-    parser.set('wallet', 'index', '0')
+    parser.add_section('addresses')
+    parser.set('wallet', 'iotaSeed', iota_seed_e.decode('utf-8'))
+    parser.set('wallet', 'index', '1')
+    parser.set('wallet', 'activeAddress', 'blank')
+    parser.set('wallet', 'balance', '0')
 
     parser.write(cfgfile)
+    print("Config file write success")
     cfgfile.close()
 
 else:
     print("Config file successfully loaded")
     index = int(parser.get('wallet', 'index'))
     iota_seed_e = parser.get('wallet', 'iotaSeed')
-    iota_seed = cipher_suite.decrypt(iota_seed_e)
+    iota_seed = cipher_suite.decrypt(iota_seed_e.encode('utf-8')).decode('utf-8')
     print(iota_seed)
+
+    api = Iota(node, iota_seed)
+    inputs = api.get_inputs()
+
+    cfgfile = open("wallet.ini", 'w')
+
+    addresses = inputs['inputs']
+    parser.set('wallet', 'index', str(len(addresses)))
+    for i, address in enumerate(addresses):
+        parser.set('addresses', 'address'+str(i), str(address))
+
+
+    inputs = api.get_inputs(start=index, stop=index+1)
+    balance = inputs['totalBalance']
+    address = inputs['inputs'][-1]
+
+    parser.set('wallet', 'balance', str(balance))
+    parser.set('wallet', 'activeAddress', str(address))
+
+    parser.write(cfgfile)
+    cfgfile.close()
+    print(inputs)
+    print(address)
+    print(balance)
+
+
+
+
+
+
+
+
+
 
